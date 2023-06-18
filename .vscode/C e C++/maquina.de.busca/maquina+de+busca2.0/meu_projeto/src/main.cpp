4fe7334d0ad6b29dcdd41987152ad58e0606eeaf
BEPROS <136277808+BEPROS@users.noreply.github.com>
2023-06-18 11:58:46 -0300

    ajustes main

--- .vscode/C e C++/maquina.de.busca/maquina+de+busca2.0/meu_projeto/src/main.cpp
+++ .vscode/C e C++/maquina.de.busca/maquina+de+busca2.0/meu_projeto/src/main.cpp
@@ -6,28 +6,25 @@ int main() {
     engine.run();
     return 0;
 }
-//Comando para compilar e rodar o código
-//g++ main.cpp index.cpp search.cpp -o meu_programa && ./meu_programa
-//Para rodar o Makefile usar make e make run 
-/*- meu_projeto
+/*Comando para compilar e rodar o código:
+g++ main.cpp index.cpp search.cpp -o meu_programa && ./meu_programa
+//Para rodar o Makefile, usar "make" (compilar) e "make run" (rodar)
+/*Estrutura do código:
+- meu_projeto
   |- Makefile
   |- src
   |   |- index.cpp
   |   |- search.cpp
   |   |- main.cpp
-  |   |- ...
   |
   |- include
   |   |- index.h
   |   |- search.h
-  |   |- ...
   |
   |- obj (gerado automaticamente)
   |
   |- bin (gerado automaticamente)
   |
   |- documentos
-      |- arquivo1.txt
-      |- arquivo2.txt
-      |- ...
+      |- ......txt
 */
