aa05adf799b11f43ad46fe55a0ed05f3567c541d
BEPROS <136277808+BEPROS@users.noreply.github.com>
2023-06-18 12:00:58 -0300

    atualizar search e explicar

--- .vscode/C e C++/maquina.de.busca/maquina+de+busca2.0/meu_projeto/src/search.cpp
+++ .vscode/C e C++/maquina.de.busca/maquina+de+busca2.0/meu_projeto/src/search.cpp
@@ -5,8 +5,11 @@ void SearchEngine::run() {
     cout << "Enter a search query (type 'quit' to exit): ";
     getline(cin, query);
     while (query != "quit") {
+        // Realiza a busca no índice invertido com base na consulta
         vector<pair<int, int>> results = index.search(query);
+        // Imprime os títulos dos documentos encontrados na busca
         index.print_titles(results, query);
+        // Solicita uma nova consulta ao usuário
         cout << "Enter a search query (type 'quit' to exit): ";
         getline(cin, query);
     }
